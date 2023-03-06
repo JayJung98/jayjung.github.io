ed559c640d11e0bfd7ed391e49d5577557eb008f
jfloww <hoon7589@gmail.com>
2023-03-06 16:45:54 -0600

    main page grid updated

--- src/components/js/Main.js
+++ src/components/js/Main.js
@@ -4,22 +4,24 @@ import '../css/Main.css'
 function main() {
     return (
         <div className='main'>
-            <div className='main_left'>
-                <img className='main_photo' src='../../images/jay01.jpg'/>
-                <div className='main_name'>
-                   Jay (Jaehoon) Jung
-                   <hr/>
+            <div className='container'>
+                <div className='main_left'>
+                    <img className='main_photo' src='../../images/jay01.jpg'/>
+                    <div className='main_name'>
+                    Jay (Jaehoon) Jung
+                    <hr/>
+                    </div>
+                    <div className='main_text'>
+                        US: TX - NY - AL <br/>
+                        KR: Seoul <br/>
+                        Software Engineer <br/>
+                        Data Scientist <br/>
+                    </div>
                 </div>
-                <div className='main_text'>
-                    US: TX - NY - AL <br/>
-                    KR: Seoul <br/>
-                    Software Engineer <br/>
-                    Data Scientist <br/>
-                </div>
-            </div>
-            <div className='side'>
-                <div className='side_text'>
-                    Welcome to my Blog
+                <div className='side'>
+                    <div className='side_text'>
+                        Welcome to my Blog
+                    </div>
                 </div>
             </div>
         </div>

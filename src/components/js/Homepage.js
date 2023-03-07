d166c140bd093cf210ca918caa34336dd931889c
jfloww <hoon7589@gmail.com>
2023-03-07 08:14:55 -0600

    grid location updated

--- src/components/js/Homepage.js
+++ src/components/js/Homepage.js
@@ -3,14 +3,12 @@ import '../css/Homepage.css'
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
-import Profile from './Profile';
 
 function homepage() {
     return (
             <div className="homepage">
             <Header/>
             <Main/>
-            <Profile/>
             {/* <img src="./images/yang01.jpg" className="App-logo" /> */}
             {/* <a
             className="App-link"
--- src/components/js/Main.js
+++ src/components/js/Main.js
@@ -1,55 +1,13 @@
 import React from 'react';
 import '../css/Main.css'
+import Profile from './Profile';
 
 function main() {
     return (
         <div className='main'>
             <div className='container'>
-                <div className='recent'>
-                    
-                </div>
-                <div className='menu_bar'>
-
-                </div>
-                <div className='test1'>
-                <div className='main_left'>
-                    <img className='main_photo' src='../../images/jay01.jpg'/>
-                    <div className='main_name'>
-                    Jay (Jaehoon) Jung
-                    <hr/>
-                    </div>
-                    <div className='main_text'>
-                        US: TX - NY - AL <br/>
-                        KR: Seoul <br/>
-                        Software Engineer <br/>
-                        Data Scientist <br/>
-                    </div>
-                </div>
-                </div>
-                <div className='test2'>
-                    
-                </div>
-                <div className='test3'>
-                    
-                </div>
-                {/* <div className='main_left'>
-                    <img className='main_photo' src='../../images/jay01.jpg'/>
-                    <div className='main_name'>
-                    Jay (Jaehoon) Jung
-                    <hr/>
-                    </div>
-                    <div className='main_text'>
-                        US: TX - NY - AL <br/>
-                        KR: Seoul <br/>
-                        Software Engineer <br/>
-                        Data Scientist <br/>
-                    </div>
-                </div>
-                <div className='side'>
-                    <div className='side_text'>
-                        Welcome to my Blog
-                    </div>
-                </div> */}
+                
+                <Profile className='profile'/>
             </div>
         </div>
     );
